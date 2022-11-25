6170c12f3e9ab5fc8bf820222c6ffa31a79402a5
Veliyeefff <6mazgz0@code.edu.az>
2022-11-25 14:24:00 +0400

    Created

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ let names = ["Yusif","Sefa","Nicat","Kamal","Aygun"]
 let surnames = ["Rzayev","Lacinov","Veliyev","Eliyeva","Haqverdiyeva"]
 let emails = ["example@gmail.com", "example@gmail.ru","example@code.edu.az"]
 let phone = [50,51,55,70,77,90,99] 
+let selection = [1,0]
 
 let input_name = document.querySelector("#nm")
 let input_surname = document.querySelector("#srnm")
 let input_email = document.querySelector("#eml")
 let input_number = document.querySelector("#phn")
 let input_date = document.querySelector("#dt")
+let male = document.querySelector("#ml")
+let female = document.querySelector("#fml")
 let btn = document.querySelector("#btn")
 
+
+console.log(typeof input_name)
 btn.addEventListener("click",function(){
     let i = Math.floor(Math.random() * names.length);
     input_name.value = names[i]
@@ -33,6 +38,16 @@ btn.addEventListener("click",function(){
     
     let m = (Math.floor(Math.random() * phone.length))
     input_number.value = '+994 '+phone[m]+" "+(Math.floor(Math.random() * 900)+100)+(Math.floor(Math.random() * 90)+10)+(Math.floor(Math.random() * 90)+10);
+
+    let s = (Math.floor(Math.random() * selection.length))
+    if(selection[s]==1){
+        male.checked = true;
+    }
+    else{
+        female.checked=true;
+    }
+
+
 })
 
 
